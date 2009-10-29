53fd2d90fec7f830494d554803cd7afb4dc5675d
Pavol Rusnak <stick@gk2.sk>
2009-10-29 23:34:13 +0100

    results working

--- result.py
+++ result.py
@@ -2,6 +2,7 @@
 
 from PyQt4 import QtCore, QtGui
 from ui_result import Ui_widgetResult
+from datetime import timedelta, datetime
 
 class WidgetResult(QtGui.QWidget):
 
@@ -14,7 +15,22 @@ class WidgetResult(QtGui.QWidget):
     def setData(self, data):
         self.vid = data['vid']
         self.ui.labelAuthor.setText( data['author'] )
-        self.ui.labelPublished.setText( data['published'] )
+        delta = datetime.now() - data['published']
+        if delta.days >= 365*2:
+            delta = str(delta.days / 365) + ' years ago'
+        elif delta.days >= 365:
+            delta = '1 year ago'
+        elif delta.days >= 30*2:
+            delta = str(delta.days / 30) + ' months ago'
+        elif delta.days >= 30:
+            delta = '1 month ago'
+        elif delta.days > 1:
+            delta = str(delta.days) + ' days ago'
+        elif delta.days == 1:
+            delta = 'yesterday'
+        else:
+            delta = 'today'
+        self.ui.labelPublished.setText( delta )
         self.ui.labelTitle.setText( data['title'] )
         self.ui.labelDesc.setText( data['desc'] )
         self.ui.labelLength.setText( "%d:%d" % (data['length'] / 60, data['length'] % 60) )
@@ -22,12 +38,13 @@ class WidgetResult(QtGui.QWidget):
         ratingwidth = int( data['rating'] * 16)
         rate0 = QtGui.QPixmap(':icons/rating0.png')
         rate1 = QtGui.QPixmap(':icons/rating1.png')
-        rating = QtGui.QPixmap(5*16,16)
+        rating = QtGui.QPixmap(80,16)
         rating.fill(QtCore.Qt.transparent)
         painter = QtGui.QPainter()
         painter.begin(rating)
-#        painter.drawPixmap(0,0,rate1,0,0,ratingwidth-1,16)
-        painter.drawPixmap(ratingwidth,0,rate0,80-ratingwidth,0,80-ratingwidth,16)
+        painter.drawPixmap(0,0,rate1,0,0,ratingwidth-1,16)
+        painter.drawPixmap(ratingwidth,0,rate0,ratingwidth,0,80-ratingwidth,16)
         painter.end()
         self.ui.imageRating.setPixmap(rating)
-        #       'thumbs': [ ('http://i.ytimg.com/vi/%s/' % vid) + str(i) + '.jpg' for i in range(1,4) ]
+        thumb = QtGui.QPixmap( data['thumbs'][0] )
+        self.ui.imageThumb.setPixmap(thumb)
--- utils.py
+++ utils.py
@@ -1,6 +1,16 @@
+import os
 import urllib
 from xml.dom import minidom
 from PyQt4 import QtGui
+from datetime import datetime
+
+confdir = os.getenv('HOME') + '/.cliptor/'
+thumbdir = confdir + 'thumbnails/'
+try:
+    os.makedirs(confdir)
+    os.makedirs(thumbdir)
+except:
+    pass
 
 class Utils():
 
@@ -22,8 +32,9 @@ class Utils():
 
     @staticmethod
     def getVideos(s):
-        params = urllib.urlencode({'max-results': 10, 'q': s})
+        params = urllib.urlencode({'max-results': 20, 'q': s})
         url = urllib.urlopen( 'http://gdata.youtube.com/feeds/api/videos?%s' % params )
+        print 'http://gdata.youtube.com/feeds/api/videos?%s' % params
         xml = url.read()
         url.close()
         dom = minidom.parseString(xml)
@@ -31,12 +42,18 @@ class Utils():
         for node in dom.getElementsByTagName('entry'):
             vid = node.getElementsByTagName('id')[0].firstChild.data.replace('http://gdata.youtube.com/feeds/api/videos/','')
             author = node.getElementsByTagName('author')[0].firstChild.firstChild.data
-            published = node.getElementsByTagName('published')[0].firstChild.data
+            published = datetime.strptime( node.getElementsByTagName('published')[0].firstChild.data[:19] , '%Y-%m-%dT%H:%M:%S')
             media = node.getElementsByTagName('media:group')[0]
             title = media.getElementsByTagName('media:title')[0].firstChild.data
-            desc = media.getElementsByTagName('media:description')[0].firstChild.data
+            if media.getElementsByTagName('media:description')[0].firstChild != None:
+                desc = media.getElementsByTagName('media:description')[0].firstChild.data
+            else:
+                desc = ''
             length = int(media.getElementsByTagName('yt:duration')[0].attributes['seconds'].value)
-            rating = float(node.getElementsByTagName('gd:rating')[0].attributes['average'].value)
+            if len(node.getElementsByTagName('gd:rating')) > 0:
+                rating = float(node.getElementsByTagName('gd:rating')[0].attributes['average'].value)
+            else:
+                rating = 0.0
             views = int(node.getElementsByTagName('yt:statistics')[0].attributes['viewCount'].value)
             result.append({
                 'vid': vid,
@@ -47,8 +64,11 @@ class Utils():
                 'length': length,
                 'rating': rating,
                 'views': views,
-                'thumbs': [ ('http://i.ytimg.com/vi/%s/' % vid) + str(i) + '.jpg' for i in range(1,4) ]
+                'thumbs': [ (thumbdir +'%s_%d.jpg') % (vid, i) for i in range(1,2) ]
             })
+            for i in range(1,2):
+                if not os.path.exists( (thumbdir +'%s_%d.jpg') % (vid, i) ):
+                    urllib.urlretrieve('http://i.ytimg.com/vi/%s/%d.jpg' % (vid, i), (thumbdir +'%s_%d.jpg') % (vid, i) )
         return result
 
     @staticmethod
